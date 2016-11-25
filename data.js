07f4ca7ff2bafa6e30b8cde01fcd5e17ffb531d6
PsMonkey <PsMonkey@dontcareabout.us>
2016-11-25 21:43:22 +0800

    更新統計數據 2016.11.25

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
-var amountData = [12, 10, 2, 0, 1, 27];
-var hardbackData = [33, 12, 9, 3];
-var anonymousData = [54, 3];
+var amountData = [12, 11, 2, 1, 2, 35];
+var hardbackData = [41, 14, 10, 3];
+var anonymousData = [64, 4];
